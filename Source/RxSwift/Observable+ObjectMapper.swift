b379b1525fd081930e39814b0cde50c37e7b2fbf
Bruno Oliveira <bruno@faber.vc>
2016-02-02 15:34:52 +0000

    Updated mapping functions to map on background and return response back to the main thread

--- Source/RxSwift/Observable+ObjectMapper.swift
+++ Source/RxSwift/Observable+ObjectMapper.swift
@@ -13,20 +13,25 @@ import ObjectMapper
 /// Extension for processing Responses into Mappable objects through ObjectMapper
 public extension ObservableType where E == Response {
 
-  /// Maps data received from the signal into an object which implements the Mappable protocol.
+  /// Maps data received from the signal into an object (on the default Background thread) which 
+  /// implements the Mappable protocol and returns the result back on the MainScheduler.
   /// If the conversion fails, the signal errors.
   public func mapObject<T: Mappable>(type: T.Type) -> Observable<T> {
-    return flatMap { response -> Observable<T> in
-      return Observable.just(try response.mapObject())
-    }
+    return observeOn(SerialDispatchQueueScheduler(globalConcurrentQueueQOS: .Background))
+      .flatMap { response -> Observable<T> in
+        return Observable.just(try response.mapObject())
+      }
+      .observeOn(MainScheduler.instance)
   }
 
-  /// Maps data received from the signal into an array of objects which implement the Mappable
-  /// protocol.
+  /// Maps data received from the signal into an array of objects (on the default Background thread)
+  /// which implement the Mappable protocol and returns the result back on the MainScheduler
   /// If the conversion fails, the signal errors.
   public func mapArray<T: Mappable>(type: T.Type) -> Observable<[T]> {
-    return flatMap { response -> Observable<[T]> in
-      return Observable.just(try response.mapArray())
-    }
+    return observeOn(SerialDispatchQueueScheduler(globalConcurrentQueueQOS: .Background))
+      .flatMap { response -> Observable<[T]> in
+        return Observable.just(try response.mapArray())
+      }
+      .observeOn(MainScheduler.instance)
   }
 }
