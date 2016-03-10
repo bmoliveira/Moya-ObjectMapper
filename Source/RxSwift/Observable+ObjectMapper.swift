cebee7a4e4416d9b4252737a657bbd220dd99c1a
robertofrontado <robertofrontado@gmail.com>
2016-03-10 16:30:18 +0100

    Removed observeOn methods
    This methods shouldn't change the observable's scheduler (observeOn), because this responsibility should be on the client side, otherwise you can end messing the client's requirements with the observable chain (self experience)

--- Source/RxSwift/Observable+ObjectMapper.swift
+++ Source/RxSwift/Observable+ObjectMapper.swift
@@ -13,25 +13,21 @@ import ObjectMapper
 /// Extension for processing Responses into Mappable objects through ObjectMapper
 public extension ObservableType where E == Response {
 
-  /// Maps data received from the signal into an object (on the default Background thread) which 
-  /// implements the Mappable protocol and returns the result back on the MainScheduler.
+  /// Maps data received from the signal into an object
+  /// which implements the Mappable protocol and returns the result back
   /// If the conversion fails, the signal errors.
   public func mapObject<T: Mappable>(type: T.Type) -> Observable<T> {
-    return observeOn(SerialDispatchQueueScheduler(globalConcurrentQueueQOS: .Background))
-      .flatMap { response -> Observable<T> in
+    return flatMap { response -> Observable<T> in
         return Observable.just(try response.mapObject())
       }
-      .observeOn(MainScheduler.instance)
   }
 
-  /// Maps data received from the signal into an array of objects (on the default Background thread)
-  /// which implement the Mappable protocol and returns the result back on the MainScheduler
+  /// Maps data received from the signal into an array of objects
+  /// which implement the Mappable protocol and returns the result back
   /// If the conversion fails, the signal errors.
   public func mapArray<T: Mappable>(type: T.Type) -> Observable<[T]> {
-    return observeOn(SerialDispatchQueueScheduler(globalConcurrentQueueQOS: .Background))
-      .flatMap { response -> Observable<[T]> in
+    return flatMap { response -> Observable<[T]> in
         return Observable.just(try response.mapArray())
       }
-      .observeOn(MainScheduler.instance)
   }
 }
