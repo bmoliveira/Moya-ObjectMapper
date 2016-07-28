4145edf951627acc893098946c0e12c33a395e8d
Mats <matsiej@gmail.com>
2016-07-28 17:17:02 +0200

    Added optional mapping of response data at non-empty keyPath

--- Source/ReactiveCocoa/SignalProducer+ObjectMapper.swift
+++ Source/ReactiveCocoa/SignalProducer+ObjectMapper.swift
@@ -7,18 +7,18 @@ extension SignalProducerType where Value == Moya.Response, Error == Moya.Error {
 
   /// Maps data received from the signal into an object which implements the Mappable protocol.
   /// If the conversion fails, the signal errors.
-  public func mapObject<T: Mappable>(type: T.Type) -> SignalProducer<T, Error> {
+  public func mapObject<T: Mappable>(type: T.Type, keyPath: String? = nil) -> SignalProducer<T, Error> {
     return producer.flatMap(.Latest) { response -> SignalProducer<T, Error> in
-      return unwrapThrowable { try response.mapObject(T) }
+		return unwrapThrowable { try response.mapObject(T, withKeyPath: keyPath) }
     }
   }
 
   /// Maps data received from the signal into an array of objects which implement the Mappable
   /// protocol.
   /// If the conversion fails, the signal errors.
-  public func mapArray<T: Mappable>(type: T.Type) -> SignalProducer<[T], Error> {
+  public func mapArray<T: Mappable>(type: T.Type, keyPath: String? = nil) -> SignalProducer<[T], Error> {
     return producer.flatMap(.Latest) { response -> SignalProducer<[T], Error> in
-      return unwrapThrowable { try response.mapArray(T) }
+      return unwrapThrowable { try response.mapArray(T, withKeyPath: keyPath) }
     }
   }
 }
--- Source/RxSwift/Observable+ObjectMapper.swift
+++ Source/RxSwift/Observable+ObjectMapper.swift
@@ -13,21 +13,21 @@ import ObjectMapper
 /// Extension for processing Responses into Mappable objects through ObjectMapper
 public extension ObservableType where E == Response {
 
-  /// Maps data received from the signal into an object
-  /// which implements the Mappable protocol and returns the result back
-  /// If the conversion fails, the signal errors.
-  public func mapObject<T: Mappable>(type: T.Type) -> Observable<T> {
-    return flatMap { response -> Observable<T> in
-        return Observable.just(try response.mapObject(T))
-      }
-  }
-
-  /// Maps data received from the signal into an array of objects
-  /// which implement the Mappable protocol and returns the result back
-  /// If the conversion fails, the signal errors.
-  public func mapArray<T: Mappable>(type: T.Type) -> Observable<[T]> {
-    return flatMap { response -> Observable<[T]> in
-        return Observable.just(try response.mapArray(T))
-      }
-  }
+	/// Maps data received from the signal into an object
+	/// which implements the Mappable protocol and returns the result back
+	/// If the conversion fails, the signal errors.
+	public func mapObject<T: Mappable>(type: T.Type, keyPath: String? = nil) -> Observable<T> {
+		return flatMap { response -> Observable<T> in
+			return Observable.just(try response.mapObject(T, withKeyPath: keyPath))
+		}
+	}
+	
+	/// Maps data received from the signal into an array of objects
+	/// which implement the Mappable protocol and returns the result back
+	/// If the conversion fails, the signal errors.
+	public func mapArray<T: Mappable>(type: T.Type, keyPath: String? = nil) -> Observable<[T]> {
+		return flatMap { response -> Observable<[T]> in
+			return Observable.just(try response.mapArray(T, withKeyPath: keyPath))
+		}
+	}
 }
