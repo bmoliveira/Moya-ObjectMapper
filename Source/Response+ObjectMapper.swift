3627bafe76ca3df02500a09efb7406965cd054d0
Dal Rupnik <legoless@gmail.com>
2016-09-14 17:39:43 +0200

    Update Response+ObjectMapper.swift

--- Source/Response+ObjectMapper.swift
+++ Source/Response+ObjectMapper.swift
@@ -14,8 +14,8 @@ public extension Response {
   /// Maps data received from the signal into an object which implements the Mappable protocol.
   /// If the conversion fails, the signal errors.
   public func mapObject<T: Mappable>(_ type: T.Type) throws -> T {
-    guard let object = Mapper<T>().map(try mapJSON()) else {
-      throw Error.JSONMapping(self)
+    guard let object = Mapper<T>().map(JSONObject: try mapJSON()) else {
+      throw Moya.Error.jsonMapping(self)
     }
    return object
   }
@@ -24,8 +24,8 @@ public extension Response {
   /// protocol.
   /// If the conversion fails, the signal errors.
   public func mapArray<T: Mappable>(_ type: T.Type) throws -> [T] {
-    guard let objects = Mapper<T>().mapArray(try mapJSON()) else {
-      throw Error.JSONMapping(self)
+    guard let objects = Mapper<T>().mapArray(JSONObject: try mapJSON()) else {
+      throw Moya.Error.jsonMapping(self)
     }
     return objects
   }
