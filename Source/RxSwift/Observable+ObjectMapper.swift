8166f44bed5486440367b32a9b29156b68a44a0b
Suyeol Jeon <devxoul@gmail.com>
2016-11-11 14:42:24 +0900

    Add RxSwift ImmutableMappable support

--- Source/RxSwift/Observable+ObjectMapper.swift
+++ Source/RxSwift/Observable+ObjectMapper.swift
@@ -31,3 +31,27 @@ public extension ObservableType where E == Response {
       }
   }
 }
+
+
+// MARK: - ImmutableMappable
+
+public extension ObservableType where E == Response {
+
+  /// Maps data received from the signal into an object
+  /// which implements the ImmutableMappable protocol and returns the result back
+  /// If the conversion fails, the signal errors.
+  public func mapObject<T: ImmutableMappable>(_ type: T.Type) -> Observable<T> {
+    return flatMap { response -> Observable<T> in
+        return Observable.just(try response.mapObject(T.self))
+      }
+  }
+
+  /// Maps data received from the signal into an array of objects
+  /// which implement the ImmutableMappable protocol and returns the result back
+  /// If the conversion fails, the signal errors.
+  public func mapArray<T: ImmutableMappable>(_ type: T.Type) -> Observable<[T]> {
+    return flatMap { response -> Observable<[T]> in
+        return Observable.just(try response.mapArray(T.self))
+      }
+  }
+}
