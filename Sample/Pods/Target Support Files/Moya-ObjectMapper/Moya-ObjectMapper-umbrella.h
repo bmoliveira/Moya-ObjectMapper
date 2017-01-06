976b82e36199db7c0061e13753fa5bf0895bd862
Scott Hoyt <scottrhoyt@gmail.com>
2017-01-06 01:04:18 -0800

    Remove sample project.

--- Sample/Pods/Target Support Files/Moya-ObjectMapper/Moya-ObjectMapper-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Moya_ObjectMapperVersionNumber;
-FOUNDATION_EXPORT const unsigned char Moya_ObjectMapperVersionString[];
-
