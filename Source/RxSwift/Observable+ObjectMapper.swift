3a2c5f1d810740969ed6ddfdfd6640d89bdfc19e
Kamil Wysocki <kamil.wysocki93@gmail.com>
2017-09-06 17:34:24 +0200

    Refactoring Observable+MoyaMapper for RxSwift.

--- Source/RxSwift/Observable+ObjectMapper.swift
+++ Source/RxSwift/Observable+ObjectMapper.swift
@@ -11,47 +11,47 @@ import Moya
 import ObjectMapper
 
 /// Extension for processing Responses into Mappable objects through ObjectMapper
-public extension ObservableType where E == Response {
+public extension Single where Element == Response {
 
-  /// Maps data received from the signal into an object
-  /// which implements the Mappable protocol and returns the result back
-  /// If the conversion fails, the signal errors.
-	public func mapObject<T: BaseMappable>(_ type: T.Type, context: MapContext? = nil) -> Observable<T> {
-    return flatMap { response -> Observable<T> in
-		return Observable.just(try response.mapObject(T.self, context: context))
-      }
-  }
+    /// Maps data received from the signal into an object
+    /// which implements the Mappable protocol and returns the result back
+    /// If the conversion fails, the signal errors.
+    public func mapObject<T:BaseMappable>(_ type: T.Type, context: MapContext? = nil) -> Observable<T> {
+        return asObservable().flatMap { response -> Observable<T> in
+            return Observable.just(try response.mapObject(T.self, context: context))
+        }
+    }
 
-  /// Maps data received from the signal into an array of objects
-  /// which implement the Mappable protocol and returns the result back
-  /// If the conversion fails, the signal errors.
-	public func mapArray<T: BaseMappable>(_ type: T.Type, context: MapContext? = nil) -> Observable<[T]> {
-    return flatMap { response -> Observable<[T]> in
-		return Observable.just(try response.mapArray(T.self, context: context))
-      }
-  }
+    /// Maps data received from the signal into an array of objects
+    /// which implement the Mappable protocol and returns the result back
+    /// If the conversion fails, the signal errors.
+    public func mapArray<T:BaseMappable>(_ type: T.Type, context: MapContext? = nil) -> Observable<[T]> {
+        return asObservable().flatMap { response -> Observable<[T]> in
+            return Observable.just(try response.mapArray(T.self, context: context))
+        }
+    }
 }
 
 
 // MARK: - ImmutableMappable
 
-public extension ObservableType where E == Response {
+public extension Single where Element == Response {
 
-  /// Maps data received from the signal into an object
-  /// which implements the ImmutableMappable protocol and returns the result back
-  /// If the conversion fails, the signal errors.
-  public func mapObject<T: ImmutableMappable>(_ type: T.Type, context: MapContext? = nil) -> Observable<T> {
-    return flatMap { response -> Observable<T> in
-		return Observable.just(try response.mapObject(T.self, context: context))
-      }
-  }
+    /// Maps data received from the signal into an object
+    /// which implements the ImmutableMappable protocol and returns the result back
+    /// If the conversion fails, the signal errors.
+    public func mapObject<T:ImmutableMappable>(_ type: T.Type, context: MapContext? = nil) -> Observable<T> {
+        return asObservable().flatMap { response -> Observable<T> in
+            return Observable.just(try response.mapObject(T.self, context: context))
+        }
+    }
 
-  /// Maps data received from the signal into an array of objects
-  /// which implement the ImmutableMappable protocol and returns the result back
-  /// If the conversion fails, the signal errors.
-  public func mapArray<T: ImmutableMappable>(_ type: T.Type, context: MapContext? = nil) -> Observable<[T]> {
-    return flatMap { response -> Observable<[T]> in
-		return Observable.just(try response.mapArray(T.self, context: context))
-      }
-  }
+    /// Maps data received from the signal into an array of objects
+    /// which implement the ImmutableMappable protocol and returns the result back
+    /// If the conversion fails, the signal errors.
+    public func mapArray<T:ImmutableMappable>(_ type: T.Type, context: MapContext? = nil) -> Observable<[T]> {
+        return asObservable().flatMap { response -> Observable<[T]> in
+            return Observable.just(try response.mapArray(T.self, context: context))
+        }
+    }
 }
