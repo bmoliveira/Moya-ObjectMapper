6098e4e434cd1c877ae6d56418a965eb3a4b8395
Kamil Wysocki <kamil.wysocki@brightinventions.pl>
2017-09-07 11:58:25 +0200

    Add support for ReactiveSwift

--- Source/ReactiveSwift/SignalProducer+ObjectMapper.swift
+++ Source/ReactiveSwift/SignalProducer+ObjectMapper.swift
@@ -3,31 +3,32 @@ import Moya
 import ObjectMapper
 
 /// Extension for processing Responses into Mappable objects through ObjectMapper
-extension SignalProducerProtocol where Value == Moya.Response, Error == Moya.Error {
+extension SignalProducerProtocol where Value == Moya.Response, Error == MoyaError {
 
-  /// Maps data received from the signal into an object which implements the Mappable protocol.
-  /// If the conversion fails, the signal errors.
-  public func mapObject<T: BaseMappable>(_ type: T.Type, context: MapContext? = nil) -> SignalProducer<T, Error> {
-    return producer.flatMap(.Latest) { response -> SignalProducer<T, Error> in
-      return unwrapThrowable { try response.mapObject(T, context) }
+    /// Maps data received from the signal into an object which implements the Mappable protocol.
+    /// If the conversion fails, the signal errors.
+    public func mapObject<T:BaseMappable>(_ type: T.Type, context: MapContext? = nil) -> SignalProducer<T, MoyaError> {
+        return producer.flatMap(.latest) {
+            (response: Response) -> SignalProducer<T, Error> in
+            return unwrapThrowable { try response.mapObject(type, context: context) }
+        }
     }
-  }
 
-  /// Maps data received from the signal into an array of objects which implement the Mappable
-  /// protocol.
-  /// If the conversion fails, the signal errors.
-  public func mapArray<T: BaseMappable>(_ type: T.Type, context: MapContext? = nil) -> SignalProducer<[T], Error> {
-    return producer.flatMap(.Latest) { response -> SignalProducer<[T], Error> in
-      return unwrapThrowable { try response.mapArray(T, context) }
+    /// Maps data received from the signal into an array of objects which implement the Mappable
+    /// protocol.
+    /// If the conversion fails, the signal errors.
+    public func mapArray<T:BaseMappable>(_ type: T.Type, context: MapContext? = nil) -> SignalProducer<[T], MoyaError> {
+        return producer.flatMap(.latest) { (response: Response) -> SignalProducer<[T], Error> in
+            return unwrapThrowable { try response.mapArray(type, context: context) }
+        }
     }
-  }
 }
 
 /// Maps throwable to SignalProducer
-private func unwrapThrowable<T>(_ throwable: () throws -> T) -> SignalProducer<T, Moya.Error> {
-  do {
-    return SignalProducer(value: try throwable())
-  } catch {
-    return SignalProducer(error: error as! Moya.Error)
-  }
+private func unwrapThrowable<T>(_ throwable: () throws -> T) -> SignalProducer<T, MoyaError> {
+    do {
+        return SignalProducer(value: try throwable())
+    } catch {
+        return SignalProducer(error: error as! MoyaError)
+    }
 }
