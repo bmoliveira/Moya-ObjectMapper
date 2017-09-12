70bde6ee92927ef4de46f697c8c533bb4b1f680f
Ivan Bruel <ivan.bruel@gmail.com>
2017-09-12 01:23:11 +0100

    Added ObservableType extension to avoid Single requirement on RxSwift

---
+++ Source/RxSwift/ObservableType+ObjectMapper.swift
@@ -0,0 +1,56 @@
+//
+//  ObserverType+ObjectMapper.swift
+//
+//  Created by Ivan Bruel on 09/12/15.
+//  Copyright © 2015 Ivan Bruel. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import Moya
+import ObjectMapper
+
+/// Extension for processing Responses into Mappable objects through ObjectMapper
+public extension ObservableType where E == Response {
+
+  /// Maps data received from the signal into an object
+  /// which implements the Mappable protocol and returns the result back
+  /// If the conversion fails, the signal errors.
+	public func mapObject<T: BaseMappable>(_ type: T.Type, context: MapContext? = nil) -> Observable<T> {
+    return flatMap { response -> Observable<T> in
+			return Observable.just(try response.mapObject(T.self, context: context))
+    }
+  }
+
+  /// Maps data received from the signal into an array of objects
+  /// which implement the Mappable protocol and returns the result back
+  /// If the conversion fails, the signal errors.
+	public func mapArray<T: BaseMappable>(_ type: T.Type, context: MapContext? = nil) -> Observable<[T]> {
+    return flatMap { response -> Observable<[T]> in
+			return Observable.just(try response.mapArray(T.self, context: context))
+    }
+  }
+}
+
+
+// MARK: - ImmutableMappable
+public extension ObservableType where E == Response {
+
+  /// Maps data received from the signal into an object
+  /// which implements the ImmutableMappable protocol and returns the result back
+  /// If the conversion fails, the signal errors.
+  public func mapObject<T: ImmutableMappable>(_ type: T.Type, context: MapContext? = nil) -> Observable<T> {
+    return flatMap { response -> Observable<T> in
+			return Observable.just(try response.mapObject(T.self, context: context))
+    }
+  }
+
+  /// Maps data received from the signal into an array of objects
+  /// which implement the ImmutableMappable protocol and returns the result back
+  /// If the conversion fails, the signal errors.
+  public func mapArray<T: ImmutableMappable>(_ type: T.Type, context: MapContext? = nil) -> Observable<[T]> {
+    return flatMap { response -> Observable<[T]> in
+			return Observable.just(try response.mapArray(T.self, context: context))
+    }
+  }
+}
--- Source/RxSwift/Single+ObjectMapper.swift
+++ Source/RxSwift/Single+ObjectMapper.swift
@@ -10,57 +10,47 @@ import RxSwift
 import Moya
 import ObjectMapper
 
-
 /// Extension for processing Responses into Mappable objects through ObjectMapper
 public extension PrimitiveSequence where TraitType == SingleTrait, ElementType == Response {
 
     /// Maps data received from the signal into an object
     /// which implements the Mappable protocol and returns the result back
     /// If the conversion fails, the signal errors.
-    public func mapObject<T:BaseMappable>(_ type: T.Type, context: MapContext? = nil) -> Single<T> {
-        return observeOn(ConcurrentDispatchQueueScheduler(qos: .background))
-                .flatMap { response -> Single<T> in
+    public func mapObject<T: BaseMappable>(_ type: T.Type, context: MapContext? = nil) -> Single<T> {
+        return flatMap { response -> Single<T> in
                     return Single.just(try response.mapObject(type, context: context))
                 }
-                .observeOn(MainScheduler.instance)
     }
 
     /// Maps data received from the signal into an array of objects
     /// which implement the Mappable protocol and returns the result back
     /// If the conversion fails, the signal errors.
-    public func mapArray<T:BaseMappable>(_ type: T.Type, context: MapContext? = nil) -> Single<[T]> {
-        return observeOn(ConcurrentDispatchQueueScheduler(qos: .background))
-                .flatMap { response -> Single<[T]> in
+    public func mapArray<T: BaseMappable>(_ type: T.Type, context: MapContext? = nil) -> Single<[T]> {
+        return flatMap { response -> Single<[T]> in
                     return Single.just(try response.mapArray(type, context: context))
                 }
-                .observeOn(MainScheduler.instance)
     }
 }
 
 
 // MARK: - ImmutableMappable
-
 public extension PrimitiveSequence where TraitType == SingleTrait, ElementType == Response {
 
     /// Maps data received from the signal into an object
     /// which implements the ImmutableMappable protocol and returns the result back
     /// If the conversion fails, the signal errors.
-    public func mapObject<T:ImmutableMappable>(_ type: T.Type, context: MapContext? = nil) -> Single<T> {
-        return observeOn(ConcurrentDispatchQueueScheduler(qos: .background))
-                .flatMap { response -> Single<T> in
+    public func mapObject<T: ImmutableMappable>(_ type: T.Type, context: MapContext? = nil) -> Single<T> {
+        return flatMap { response -> Single<T> in
                     return Single.just(try response.mapObject(type, context: context))
                 }
-                .observeOn(MainScheduler.instance)
     }
 
     /// Maps data received from the signal into an array of objects
     /// which implement the ImmutableMappable protocol and returns the result back
     /// If the conversion fails, the signal errors.
-    public func mapArray<T:ImmutableMappable>(_ type: T.Type, context: MapContext? = nil) -> Single<[T]> {
-        return observeOn(ConcurrentDispatchQueueScheduler(qos: .background))
-                .flatMap { response -> Single<[T]> in
+    public func mapArray<T: ImmutableMappable>(_ type: T.Type, context: MapContext? = nil) -> Single<[T]> {
+        return flatMap { response -> Single<[T]> in
                     return Single.just(try response.mapArray(type, context: context))
                 }
-                .observeOn(MainScheduler.instance)
     }
 }
