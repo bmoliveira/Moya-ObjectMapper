4dca1d35a4e66fa26f4cf542e8243acfe33d56e2
dangthaison91 <dangthaison.91@gmail.com>
2018-02-21 15:34:14 +0700

    Remove ImmutableMappable code duplication

--- Source/RxSwift/ObservableType+ObjectMapper.swift
+++ Source/RxSwift/ObservableType+ObjectMapper.swift
@@ -31,26 +31,3 @@ public extension ObservableType where E == Response {
     }
   }
 }
-
-
-// MARK: - ImmutableMappable
-public extension ObservableType where E == Response {
-
-  /// Maps data received from the signal into an object
-  /// which implements the ImmutableMappable protocol and returns the result back
-  /// If the conversion fails, the signal errors.
-  public func mapObject<T: ImmutableMappable>(_ type: T.Type, context: MapContext? = nil) -> Observable<T> {
-    return flatMap { response -> Observable<T> in
-			return Observable.just(try response.mapObject(T.self, context: context))
-    }
-  }
-
-  /// Maps data received from the signal into an array of objects
-  /// which implement the ImmutableMappable protocol and returns the result back
-  /// If the conversion fails, the signal errors.
-  public func mapArray<T: ImmutableMappable>(_ type: T.Type, context: MapContext? = nil) -> Observable<[T]> {
-    return flatMap { response -> Observable<[T]> in
-			return Observable.just(try response.mapArray(T.self, context: context))
-    }
-  }
-}
