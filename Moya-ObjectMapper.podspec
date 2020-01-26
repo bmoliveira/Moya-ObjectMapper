1f64732e80f70e54cbe8f901d79475a06be2768b
Bruno Oliveira <bm.oliveira.dev@gmail.com>
2020-01-26 16:34:51 +0000

    Bump versions and

--- Moya-ObjectMapper.podspec
+++ Moya-ObjectMapper.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name         = "Moya-ObjectMapper"
-  s.version      = "2.9"
+  s.version      = "3.0"
   s.summary      = "ObjectMapper bindings for Moya"
   s.description  = <<-EOS
   [ObjectMapper](https://github.com/Hearst-DD/ObjectMapper) bindings for
@@ -19,18 +19,19 @@ Pod::Spec.new do |s|
   s.tvos.deployment_target = '10.0'
   s.source       = { :git => "https://github.com/bmoliveira/Moya-ObjectMapper.git", :tag => s.version }
   s.default_subspec = "Core"
+  s.swift_version = '5.0'
 
   s.subspec "Core" do |ss|
     ss.source_files  = "Source/Core/*.swift"
-    ss.dependency "Moya"
-    ss.dependency "ObjectMapper", "~> 3.5.1"
+    ss.dependency "Moya", '~> 14.0.0-beta.6'
+    ss.dependency "ObjectMapper"
     ss.framework  = "Foundation"
   end
 
   s.subspec "RxSwift" do |ss|
     ss.source_files = "Source/RxSwift/*.swift"
     ss.dependency "Moya-ObjectMapper/Core"
-    ss.dependency "RxSwift", "~> 5"
+    ss.dependency "RxSwift"
     ss.dependency "Moya/RxSwift"
   end
 
@@ -38,6 +39,6 @@ Pod::Spec.new do |s|
     ss.source_files = "Source/ReactiveSwift/*.swift"
     ss.dependency "Moya-ObjectMapper/Core"
     ss.dependency "Moya/ReactiveSwift"
-    ss.dependency "ReactiveSwift" , "~> 6.0"
+    ss.dependency "ReactiveSwift"
   end
 end
--- Source/Core/Response+ObjectMapper.swift
+++ Source/Core/Response+ObjectMapper.swift
@@ -13,7 +13,7 @@ public extension Response {
 
   /// Maps data received from the signal into an object which implements the Mappable protocol.
   /// If the conversion fails, the signal errors.
-	public func mapObject<T: BaseMappable>(_ type: T.Type, context: MapContext? = nil) throws -> T {
+    func mapObject<T: BaseMappable>(_ type: T.Type, context: MapContext? = nil) throws -> T {
 		guard let object = Mapper<T>(context: context).map(JSONObject: try mapJSON()) else {
       throw MoyaError.jsonMapping(self)
     }
@@ -23,7 +23,7 @@ public extension Response {
   /// Maps data received from the signal into an array of objects which implement the Mappable
   /// protocol.
   /// If the conversion fails, the signal errors.
-  public func mapArray<T: BaseMappable>(_ type: T.Type, context: MapContext? = nil) throws -> [T] {
+    func mapArray<T: BaseMappable>(_ type: T.Type, context: MapContext? = nil) throws -> [T] {
 	guard let array = try mapJSON() as? [[String : Any]] else {
       throw MoyaError.jsonMapping(self)
     }
@@ -32,7 +32,7 @@ public extension Response {
 
   /// Maps data received from the signal into an object which implements the Mappable protocol.
   /// If the conversion fails, the signal errors.
-  public func mapObject<T: BaseMappable>(_ type: T.Type, atKeyPath keyPath: String, context: MapContext? = nil) throws -> T {
+    func mapObject<T: BaseMappable>(_ type: T.Type, atKeyPath keyPath: String, context: MapContext? = nil) throws -> T {
     guard let object = Mapper<T>(context: context).map(JSONObject: (try mapJSON() as? NSDictionary)?.value(forKeyPath: keyPath)) else {
       throw MoyaError.jsonMapping(self)
     }
@@ -42,7 +42,7 @@ public extension Response {
   /// Maps data received from the signal into an array of objects which implement the Mappable
   /// protocol.
   /// If the conversion fails, the signal errors.
-  public func mapArray<T: BaseMappable>(_ type: T.Type, atKeyPath keyPath: String, context: MapContext? = nil) throws -> [T] {
+    func mapArray<T: BaseMappable>(_ type: T.Type, atKeyPath keyPath: String, context: MapContext? = nil) throws -> [T] {
     guard let array = (try mapJSON() as? NSDictionary)?.value(forKeyPath: keyPath) as? [[String : Any]] else {
       throw MoyaError.jsonMapping(self)
     }
@@ -58,14 +58,14 @@ public extension Response {
   /// Maps data received from the signal into an object which implements the ImmutableMappable
   /// protocol.
   /// If the conversion fails, the signal errors.
-  public func mapObject<T: ImmutableMappable>(_ type: T.Type, context: MapContext? = nil) throws -> T {
+    func mapObject<T: ImmutableMappable>(_ type: T.Type, context: MapContext? = nil) throws -> T {
 		return try Mapper<T>(context: context).map(JSONObject: try mapJSON())
   }
 
   /// Maps data received from the signal into an array of objects which implement the ImmutableMappable
   /// protocol.
   /// If the conversion fails, the signal errors.
-  public func mapArray<T: ImmutableMappable>(_ type: T.Type, context: MapContext? = nil) throws -> [T] {
+    func mapArray<T: ImmutableMappable>(_ type: T.Type, context: MapContext? = nil) throws -> [T] {
     guard let array = try mapJSON() as? [[String : Any]] else {
       throw MoyaError.jsonMapping(self)
     }
@@ -75,7 +75,7 @@ public extension Response {
   /// Maps data received from the signal into an object which implements the ImmutableMappable
   /// protocol.
   /// If the conversion fails, the signal errors.
-  public func mapObject<T: ImmutableMappable>(_ type: T.Type, atKeyPath keyPath: String, context: MapContext? = nil) throws -> T {
+    func mapObject<T: ImmutableMappable>(_ type: T.Type, atKeyPath keyPath: String, context: MapContext? = nil) throws -> T {
     guard let object = Mapper<T>(context: context).map(JSONObject: (try mapJSON() as? NSDictionary)?.value(forKeyPath: keyPath)) else {
       throw MoyaError.jsonMapping(self)
     }
@@ -85,7 +85,7 @@ public extension Response {
   /// Maps data received from the signal into an array of objects which implement the ImmutableMappable
   /// protocol.
   /// If the conversion fails, the signal errors.
-  public func mapArray<T: ImmutableMappable>(_ type: T.Type, atKeyPath keyPath: String, context: MapContext? = nil) throws -> [T] {
+    func mapArray<T: ImmutableMappable>(_ type: T.Type, atKeyPath keyPath: String, context: MapContext? = nil) throws -> [T] {
     guard let array = (try mapJSON() as? NSDictionary)?.value(forKeyPath: keyPath) as? [[String : Any]] else {
       throw MoyaError.jsonMapping(self)
     }
